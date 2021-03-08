67d7e50603f901aa16bb1d53245a667c0276edc7
Dobrin Donev <dobrin.donev@taryafintech.com>
2021-03-08 17:48:08 +0200

    HTML + JS + CSS

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const body = document.body;
-
+const ul = document.querySelector('#my-list')
 
 function addItem() {
+    const input = document.querySelector('#input')
+    const li = document.createElement('li');
 
+    li.innerText = input.value;
+
+    ul.appendChild(li);
+
+    input.value = '';
 }
 
